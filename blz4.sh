ff6314f9ee071d8438be39aec8fefdc1bfbb1b0a
Miguel Martinez <slverwolf@gmail.com>
2018-02-20 09:13:31 -0600

    build: Import build scripts and folder

---
+++ blz4.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+############################################################
+### Build script for HolyDragon kernel ###
+############################################################
+
+# This is the full build script used to build the official kernel zip.
+
+# Minimum requirements to build:
+# Already working build environment :P 
+#
+# In this script: 
+# You will need to change the 'Source path to kernel tree' to match your current path to this source.
+# You will need to change the 'Compile Path to out' to match your current path to this source.
+# You will also need to edit the '-j32' under 'Start Compile' section and adjust that to match the amount of cores you want to use to build.
+# 
+# In Makefile: 
+# You will need to edit the 'CROSS_COMPILE=' line to match your current path to this source.
+# 
+# Once those are done, you should be able to execute './build.sh' from terminal and receive a working zip.
+
+############################################################
+# Build Script Variables
+############################################################ 
+
+# Source defconfig used to build
+	dc=HDO_defconfig
+
+# Source Path to kernel tree
+	k=/home/holyangel/android/Marlin
+
+# Source Path to clean(empty) out folder
+	co=$k/out
+
+# Compile Path to out 
+	o="O=/home/holyangel/android/Marlin/out"
+
+# Source Path to compiled Image.gz-dtb
+	i=$k/out/arch/arm64/boot/Image.lz4-dtb
+
+# Destination Path for compiled modules
+	zm=$k/build/modules
+
+# Destination path for compiled Image.gz-dtb
+	zi=$k/build/Image.lz4-dtb
+	
+# Source path for building kernel zip
+	zp=$k/build/
+	
+# Destination Path for uploading kernel zip
+	zu=$k/upload/
+
+# Kernel zip Name
+##############################
+	kn=HDK_Marlin_AK2_V0.0.0.2.zip
+
+############################################################
+# Cleanup
+############################################################
+
+	echo "	Cleaning up out directory"
+	rm -Rf out/
+	rm -f build/Image.gz-dtb
+	rm -f build/Image.lz4-dtb
+	echo "	Out directory removed!"
+
+############################################################
+# Make out folder
+############################################################
+
+	echo "	Making new out directory"
+	mkdir -p "$co"
+	echo "	Created new out directory"
+
+############################################################
+# Establish defconfig
+############################################################
+
+	echo "	Establishing build environment.."
+	make "$o" "$dc"
+
+############################################################
+# Start Compile
+############################################################
+
+	echo "	First pass started.."
+	make "$o" -j32
+	echo "	First pass completed!"
+	echo "	"
+	echo "	Starting Second Pass.."
+	make "$o" -j32
+	echo "	Second pass completed!"
+
+############################################################
+# Copy image.gz-dtb to /build
+############################################################
+
+	echo "	Copying kernel to zip directory"
+	cp "$i" "$zi"
+#	find . -name "*.ko" -exec cp {} "$zm" \;
+	echo "	Copying kernel completed!"
+	
+############################################################
+# Make zip and move to /upload
+############################################################
+
+	echo "	Making zip file.."
+	cd "$zp"
+	zip -r "$kn" *
+	echo "	Moving zip to upload directory"
+	mv "$kn" "$zu" 
+	echo "	Completed build script!"
+	echo "	Returning to start.."
+	cd "$k"
---
+++ build/anykernel.sh
@@ -0,0 +1,40 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=-- HolyDragon -- Android 8.1.0 --
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+do.debug=0
+device.name1=sailfish
+device.name2=marlin
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## AnyKernel install
+split_boot;
+
+# ramdisk changes
+flash_boot;
+
+## end install
+
