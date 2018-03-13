a7729bf1eaf6495dd120ee7970901bc745d48a24
Miguel Martinez <slverwolf@gmail.com>
2018-03-13 12:21:08 -0500

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 ############################################################
 ### Build script for HolyDragon kernel ###
 ############################################################
-
+ 
 # This is the full build script used to build the official kernel zip.
-
+ 
 # Minimum requirements to build:
 # Already working build environment :P 
 #
@@ -17,95 +17,96 @@
 # You will need to edit the 'CROSS_COMPILE=' line to match your current path to this source.
 # 
 # Once those are done, you should be able to execute './build.sh' from terminal and receive a working zip.
-
+ 
 ############################################################
 # Build Script Variables
 ############################################################ 
-
+ 
 # Source defconfig used to build
-	dc=HDO_defconfig
-
+  dc=HDO_defconfig
+ 
 # Source Path to kernel tree
-	k=/home/holyangel/android/msm8996
-
+  k=/home/holyangel/android/Marlin
+ 
 # Source Path to clean(empty) out folder
-	co=$k/out
-
+  co=$k/out
+ 
 # Compile Path to out 
-	o="O=/home/holyangel/android/msm8996/out"
-
+  o="O=/home/holyangel/android/Marlin/out"
+ 
 # Source Path to compiled Image.gz-dtb
-	i=$k/out/arch/arm64/boot/Image.gz-dtb
-
+  i=$k/out/arch/arm64/boot/Image.lz4-dtb
+ 
 # Destination Path for compiled modules
-	zm=$k/build/system/lib/modules
-
+  zm=$k/build/system/lib/modules
+ 
 # Destination path for compiled Image.gz-dtb
-	zi=$k/build/kernel/Image.gz-dtb
-	
+  zi=$k/build/Image.lz4-dtb
+  
 # Source path for building kernel zip
-	zp=$k/build/
-	
+  zp=$k/build/
+  
 # Destination Path for uploading kernel zip
-	zu=$k/upload/
-
+  zu=$k/upload/
+ 
 # Kernel zip Name
 ##############################
-	kn=HDK_OP3_AK2_OMNI_V0.0.4.3.zip
-
+  kn=HDK_Marlin_AK2_V0.0.0.6.2.zip
+ 
 ############################################################
 # Cleanup
 ############################################################
-
-	echo "	Cleaning up out directory"
-	rm -Rf out/
-	echo "	Out directory removed!"
-
+ 
+  echo "  Cleaning up out directory"
+  rm -Rf out/
+  echo "  Out directory removed!"
+ 
 ############################################################
 # Make out folder
 ############################################################
-
-	echo "	Making new out directory"
-	mkdir -p "$co"
-	echo "	Created new out directory"
-
+ 
+  echo "  Making new out directory"
+  mkdir -p "$co"
+  echo "  Created new out directory"
+ 
 ############################################################
 # Establish defconfig
 ############################################################
-
-	echo "	Establishing build environment.."
-	make "$o" "$dc"
-
+ 
+  echo "  Establishing build environment.."
+  make "$o" "$dc"
+ 
 ############################################################
 # Start Compile
 ############################################################
-
-	echo "	First pass started.."
-	make "$o" -j32
-	echo "	First pass completed!"
-	echo "	"
-	echo "	Starting Second Pass.."
-	make "$o" -j32
-	echo "	Second pass completed!"
-
+ 
+  echo "  First pass started.."
+  make "$o" -j32
+  echo "  First pass completed!"
+  echo "  "
+  echo "  Starting Second Pass.."
+  make "$o" -j32
+  echo "  Second pass completed!"
+ 
 ############################################################
 # Copy image.gz-dtb to /build
 ############################################################
-
-	echo "	Copying kernel to zip directory"
-	cp "$i" "$zi"
-#	find . -name "*.ko" -exec cp {} "$zm" \;
-	echo "	Copying kernel completed!"
-	
+ 
+  echo "  Copying kernel to zip directory"
+  cp "$i" "$zi"
+#  find . -name "*.ko" -exec cp {} "$zm" \;
+  echo "  Copying kernel completed!"
+  
 ############################################################
 # Make zip and move to /upload
 ############################################################
+ 
+  echo "  Making zip file.."
+  cd "$zp"
+  zip -r "$kn" *
+  echo "  Moving zip to upload directory"
+  mv "$kn" "$zu" 
+  echo "  Completed build script!"
+  echo "  Returning to start.."
+  cd "$k"
 
-	echo "	Making zip file.."
-	cd "$zp"
-	zip -r "$kn" *
-	echo "	Moving zip to upload directory"
-	mv "$kn" "$zu" 
-	echo "	Completed build script!"
-	echo "	Returning to start.."
-	cd "$k"
